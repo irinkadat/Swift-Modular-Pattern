67195af8dddeadf22d7e8059e917fb80272e4564
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-05-03 08:58:51 +0400

    Initial Commit

---
+++ Swift Modular Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Modular Pattern
+//
+//  Created by Irinka Datoshvili on 03.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
